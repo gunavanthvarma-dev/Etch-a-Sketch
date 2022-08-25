072f0f34415e3b8bc7edfce984b6ca67e0225f31
gunavanthdev <gunavanthvarma.dev@gmail.com>
2022-08-25 21:59:43 +0530

    added eventlistener that registers mouseover event

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ let container = document.querySelector(".container");
 for(let i=0;i<256;i++)
 {
     let div = document.createElement('div');
-    div.textContent = "div";
+    //div.textContent = "div";
+    div.style.backgroundColor = 'bisque';
     div.classList.add("col");
     container.appendChild(div);
 }
 
+
+let draw = document.querySelectorAll('.col');
+draw.forEach(function(dra)
+{
+    dra.addEventListener('mouseover',function (e)
+    {
+        e.target.style.backgroundColor = 'black';
+    }); 
+    
+});
+
+/*draw.addEventListener('mouseleave',function (e)
+{
+    e.target.style.background = 'black';
+});*/
