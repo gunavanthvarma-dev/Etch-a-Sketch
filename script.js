3c02208f1ba28ee958870ce00cb99f4ee0113615
gunavanthdev <gunavanthvarma.dev@gmail.com>
2022-08-25 23:42:19 +0530

    added options button in html file,css styles for the button was added, js code for sketch,eraser,clear was added

--- script.js
+++ script.js
@@ -8,18 +8,54 @@ for(let i=0;i<256;i++)
     container.appendChild(div);
 }
 
+let sketch = document.querySelector('#sketch');
+sketch.addEventListener('click',function(e)
+{
+    let draw = document.querySelectorAll('.col');
+    draw.forEach(function(dra)
+   {
+    dra.addEventListener('mouseover',function (e)
+    {
+        e.target.style.backgroundColor = 'black';
+    }); 
 
-let draw = document.querySelectorAll('.col');
+   });
+});
+
+let eraser = document.querySelector('#eraser')
+eraser.addEventListener('click',function(e)
+{
+    let erase = document.querySelectorAll('.col');
+    erase.forEach(function(era)
+   {
+    era.addEventListener('mouseover',function (e)
+    {
+        e.target.style.backgroundColor = 'bisque';
+    }); 
+
+   });
+});
+
+let clear = document.querySelector('#clear')
+clear.addEventListener('click',function(e)
+{
+    let cle = document.querySelectorAll('.col');
+    cle.forEach(function(cl)
+   {
+      cl.style.backgroundColor = 'bisque';
+   });
+});
+
+
+
+/*let draw = document.querySelectorAll('.col');
 draw.forEach(function(dra)
 {
     dra.addEventListener('mouseover',function (e)
     {
         e.target.style.backgroundColor = 'black';
     }); 
-    
-});
 
-/*draw.addEventListener('mouseleave',function (e)
-{
-    e.target.style.background = 'black';
 });*/
+
+
